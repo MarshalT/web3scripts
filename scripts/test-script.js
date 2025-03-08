3bb12d356200a92fa42ff0e310efac9104eafdeb
jianhong tang <602032825@qq.com>
2025-03-08 22:18:10 +0800

    test

--- scripts/test-script.js
+++ scripts/test-script.js
@@ -3,6 +3,7 @@ function testFunction(walletData, args) {
     console.log('测试函数执行成功');
     console.log('参数:', args);
     return {
+        success: true,
         message: '测试成功',
         time: new Date().toISOString()
         //123456
